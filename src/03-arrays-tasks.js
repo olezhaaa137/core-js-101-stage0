f4de39c5e91b791ff95ecfc18e3db04e2311d765
Oleg Byiko <oleshkabyiko@gmail.com>
2026-05-11 00:42:40 +0300

    feat: new features

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -471,8 +471,10 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array(end - start + 1)
+    .fill(0)
+    .map((_, index) => start + index);
 }
 
 /**
