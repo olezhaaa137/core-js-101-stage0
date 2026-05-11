cd755d7e309bb221ec23e20609dbbff6c60402e0
Oleg Byiko <oleshkabyiko@gmail.com>
2026-05-11 16:04:44 +0300

    feat: add new features

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -534,7 +534,6 @@ function group(array, keySelector, valueSelector) {
 
     return mapa;
   }, new Map());
-  
   return result;
 }
 /**
@@ -569,8 +568,8 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((acc, item) => acc[item], arr);
 }
 
 /**
@@ -591,9 +590,18 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  const centerElement =
+    arr.length % 2 === 0 ? [] : [arr[Math.floor(arr.length / 2)]];
+  return arr
+    .slice(-arr.length / 2)
+    .concat(centerElement)
+    .concat(arr.slice(0, arr.length / 2));
 }
+swapHeadAndTail([1, 2, 3]);
 
 module.exports = {
   findElement,
