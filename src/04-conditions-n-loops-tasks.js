95d457c9baffe2ae8dd9a216a2cea8ffd8dd3bd7
Oleg Byiko <oleshkabyiko@gmail.com>
2026-05-12 20:37:59 +0300

    feat:

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -26,8 +26,18 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz';
+  }
+  if (num % 3 === 0) {
+    return 'Fizz';
+  }
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
+
+  return num;
 }
 
 /**
@@ -41,10 +51,13 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
-}
+function getFactorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
 
+  return n * getFactorial(n - 1);
+}
 /**
  * Returns the sum of integer numbers between n1 and n2 (inclusive).
  *
